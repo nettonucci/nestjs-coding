94fd940bb0c1214b982deb32bdc6cbed682883e9
José Nucci Netto <nettonucci@gmail.com>
2021-10-29 08:54:46 -0300

    Criando interface e dtos de jogadores

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
 
     @Post()
-    async criarAtualizarJogador() {
+    async criarAtualizarJogador(
+        @Body() criarJogadorDto: CriarJogadorDto
+    ) {
+        const { email } = criarJogadorDto
         return JSON.stringify({
-            "nome": "Netto"
+            "email": email
         })
     }
 }
